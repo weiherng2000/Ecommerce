6c688a836a81471873fdad1ec1f22a206546cdea
Tan wei herng <weiherng2000@gmail.com>
2023-08-01 23:18:00 +0800

    ???

--- pages/api/products.js
+++ pages/api/products.js
@@ -8,6 +8,19 @@ export default async function handle(req,res)
     const {method} = req;
     //we connect to our database
     await mongooseConnect();
+
+    if (method === 'GET') {
+
+      if (req.query?.id) 
+      {
+        res.json(await mongoose.model("Product").findOne({_id:req.query.id}));
+      } 
+      else 
+      {
+        res.json(await mongoose.model("Product").find());
+      }
+
+    }
     if(method === "POST")
     {
        const {title,description,price} = req.body;
--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,45 @@
 import Layout from "@/components/Layout"
 import Link from 'next/link'
+import { useEffect,useState} from "react";
+import axios from 'axios';
 
 export default function Products()
 {
+    const [products,setProducts] = useState([]);
+    useEffect(() => {
+      axios.get('/api/products').then(response => {
+        setProducts(response.data);
+      });
+    }, []);
     return(
         <Layout>
             <Link  className = "bg-blue-900 text-white rounded-md py-1 px-2" href = {'/products/new'}>Add new product</Link>
+            <table className = "basic mt-2">
+                <thead>
+                    <tr>
+                        <td>Product name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr>
+                            <td>{product.title}</td>
+                            <td>
+                                <Link href = {'/products/edit/' + product._id}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                </svg>
+
+                                    Edit
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+                
+            </table>
+       
         </Layout>
     );
 }
